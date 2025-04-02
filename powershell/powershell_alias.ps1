1232382e69eed7e0d068e56c3d1c7bcb45cea8e6
jkko_yoga <jkwonko@gmail.com>
2025-04-02 12:36:25 +0900

    Update powershell_alias.ps

--- powershell/powershell_alias.ps1
+++ powershell/powershell_alias.ps1
@@ -5,7 +5,7 @@ function llf { Get-ChildItem -File} # list files only
 function lla { Get-ChildItem -Attributes Hidden, !Hidden} # list hidden and non-hidden files
 
 ## eza command
-function tree { & eza -lh --icons=auto --tree --level=$args}
+function tree { & eza -h --icons=auto --no-permission --tree --level=$args}
 
 # copying current folder name
 function cpwd { (pwd).path | Set-Clipboard }
@@ -28,3 +28,22 @@ function gac { & git add --all ; & git commit -m $args }
 Set-Alias -Name vi -Value nvim
 function ink { & inkscape $args }
 function draw { & draw.io $args }
+
+# fzf commands
+# using fzf find process ID and copy to clip board
+# change [5] for ID column number accordingly 
+function findp { Get-Process | fzf | ForEach-Object { ($_ -split '\s+')[5].Trim() } | Set-Clipboard }
+# Create stop process function
+function stopp {
+        param( [int]$processId )
+        try {
+            # Attempt to stop the process
+            Stop-Process -Id $processId -Force
+            Write-Host "Process $processId has been terminated." -ForegroundColor Green
+        }
+        catch {
+            # Handle errors
+            Write-Host "Failed to terminate the process: $($_.Exception.Message)" -ForegroundColor Red
+        }
+}
+
